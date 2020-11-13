97a9253d90f1e1d45d2c01023a532cc807f601b5
Uetani <satoshi.0411@icloud.com>
2020-11-13 13:24:23 +0900

    [add] tasksテーブル nameカラムに30文字制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_071249) do
+ActiveRecord::Schema.define(version: 2020_11_13_042032) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
