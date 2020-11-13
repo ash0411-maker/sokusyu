8fe01e8e693d9f7bb475d191ae0a167c07d0ba36
Uetani <satoshi.0411@icloud.com>
2020-11-13 16:53:11 +0900

    [add] Admin::Users CRUD機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,47 +1,55 @@
-class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+class Admin::UsersController < ApplicationController
+  before_action :require_admin
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks.recent
+    @users = User.all
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @user = User.new
   end
 
   def edit
   end
 
   def create
-    @task = current_user.tasks.new(task_params)
+    @user = User.new(user_params)
 
-    if @task.save
-      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
   end
 
   def destroy
-    @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
   end
 
   private
 
-  def task_params
-    params.require(:task).permit(:name, :description)
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 
-  def set_task
-    @task = current_user.tasks.find(params[:id])
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_admin
+    
   end
 end
