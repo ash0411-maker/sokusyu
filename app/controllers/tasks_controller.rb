a17e93b2428edb036a1b152691de8d791da779c1
Uetani <satoshi.0411@icloud.com>
2020-11-17 08:57:24 +0900

    confirm back button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,12 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to tasks_path, notice: "タスク「#{@task.name}」を登録しました！"
       # redirect_to tasks_path
